ca0137a06a1264984a65fe147938860c466951a6
Axdiscovery <623857266@qq.com>
2017-11-17 19:48:27 +0800

    shopcartsJS

---
+++ 商品列表.sql
@@ -0,0 +1,50 @@
+/*
+* @Author: 王zhichao
+* @Date:   2017-10-18 15:22:53
+* @Last Modified by:   王zhichao
+* @Last Modified time: 2017-10-18 15:24:15
+*/
+create table goodsinfo
+(
+       goodsId varchar(10) primary key,
+       goodsName varchar(100),
+       goodsType varchar(20),
+       goodsPrice float,
+       goodsCount int,
+       goodsDesc varchar(100),
+       goodsImg varchar(100),
+       beiyong1  varchar(100),
+       beiyong2  varchar(100),
+       beiyong3  varchar(100),
+       beiyong4  varchar(100),
+       beiyong5  varchar(100),
+       beiyong6  varchar(100),
+       beiyong7  varchar(100),
+       beiyong8  varchar(100),
+       beiyong9  varchar(100),
+       beiyong10  varchar(100),
+       beiyong11  varchar(100),
+       beiyong12  varchar(100),
+       beiyong13  varchar(100)
+);
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("8","张九","6999","3888","","包邮","","images/list2/1.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("9","张","6999","3888","","包邮","","images/list2/2.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("10","张九","6999","3888","","包邮","","images/list2/3.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("11","张九","6999","3888","","包邮","","images/list2/4.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("12","张九","6999","3888","","包邮","","images/list2/5.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("13","张九","6999","3888","","包邮","","images/list2/6.jpg","去看看","","","","","","","","","","","");
+insert into goodsinfo (goodsId, goodsName ,goodsType , goodsPrice ,goodsCount,goodsDesc,goodsImg,beiyong1,beiyong2
+,beiyong3,beiyong4,beiyong5,beiyong6,beiyong7,beiyong8,beiyong9,beiyong10,beiyong11,beiyong12,beiyong13) values 
+("14","张九","6999","3888","","包邮","","images/list2/7.jpg","去看看","","","","","","","","","","","");
